0f16be1a9523243e89cc97846714f3ee8c03262e
Axel Cymerman <axelcy@gmail.com>
2024-01-31 15:38:18 -0300

    development / production changes

--- src/registerCommands.ts
+++ src/registerCommands.ts
@@ -5,16 +5,23 @@ export default async(commands: Object[], client: Client) => {
     try {
         if (!process.env.TOKEN) return console.error('TOKEN not found in .env file.')
         if (!process.env.CLIENT_ID) return console.error('CLIENT_ID not found in .env file.')
-        const CLIENT_ID = process.env.CLIENT_ID
+        if (!process.env.GUILD_ID) return console.error('GUILD_ID not found in .env file.')
+        // const CLIENT_ID = process.env.CLIENT_ID
         const rest = new REST({ version: '10' }).setToken(process.env.TOKEN)
         console.log('Registering slash commands...')
-        client.guilds.cache.map(async(guild) => 
+        if (process.env.NODE_ENV === 'development') {
             await rest.put(
-                Routes.applicationGuildCommands(CLIENT_ID, guild.id), // process.env.GUILD_ID
+                Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
                 { body: commands }
             )
-        )
-        console.log('Slash commands were registered successfully!')
+        }
+        else if (process.env.NODE_ENV === 'production') {
+            await rest.put(
+                Routes.applicationCommands(process.env.CLIENT_ID),
+                { body: commands }
+            )
+        }
+        console.log('Slash commands were registered successfully as: ' + process.env.NODE_ENV)
     } catch (error) {
         console.error('Register commands error: ' + error)
     }
