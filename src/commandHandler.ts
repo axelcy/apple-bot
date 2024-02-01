67c0f89f1d149ce9007e12a1e1c07f10c2904dcb
Axel Cymerman <axelcy@gmail.com>
2024-01-31 21:10:34 -0300

    fixing build version

--- src/commandHandler.ts
+++ src/commandHandler.ts
@@ -4,11 +4,15 @@ import getAllFiles from "./utils/getAllFiles.js"
 
 export default async(client: Client) => {
     try {
-        const commandFiles = getAllFiles('src/commands')
-        // const importedModules: { name: string, callback: Function }[] = []
+        if (!process.env.INDEX_FILE) return console.error('INDEX_FILE environment variable not set.')
+        const INDEX_FILE_FOLDER = process.env.INDEX_FILE.split('/')[0]
+        const commandFiles = getAllFiles(`${INDEX_FILE_FOLDER}/commands`)
+        if (!commandFiles) return console.error('No command files found.')
+
         const importedModules: { slashCommand: SlashCommandBuilder, callback: Function }[] = []
         for (const commandFile of commandFiles) {
-            importedModules.push((await import(`./${commandFile.split('src/')[1]}`)).default)
+            console.log(`Importing ${commandFile}`)
+            importedModules.push((await import(`./${commandFile.split(`${INDEX_FILE_FOLDER}/`)[1]}`)).default)
         }
         await registerCommands(importedModules.map(module => module.slashCommand), client)
         client.on('interactionCreate', async (interaction) => {
--- src/utils/getAllFiles.ts
+++ src/utils/getAllFiles.ts
@@ -2,15 +2,19 @@ import { readdirSync } from 'fs'
 import { join } from 'path'
 
 export default (directory: string, foldersOnly = false) => {
-    let fileNames = []
-    const files = readdirSync(directory, { withFileTypes: true })
+    try {
+        let fileNames = []
+        const files = readdirSync(directory, { withFileTypes: true })
 
-    for (const file of files) {
-        const filePath = join(directory, file.name)
-        if (foldersOnly && file.isDirectory()) fileNames.push(filePath)
-        else if (file.isFile()) fileNames.push(filePath)
+        for (const file of files) {
+            const filePath = join(directory, file.name)
+            if (foldersOnly && file.isDirectory()) fileNames.push(filePath)
+            else if (file.isFile()) fileNames.push(filePath)
+        }
+        fileNames = fileNames.map(fileName => fileName.replace(/\\/g, '/'))
+
+        return fileNames
+    } catch (error) {
+        console.error('Error getting all files: ' + error)
     }
-    fileNames = fileNames.map(fileName => fileName.replace(/\\/g, '/'))
-    
-    return fileNames
 }
