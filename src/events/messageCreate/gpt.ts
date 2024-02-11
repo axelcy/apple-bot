c19eb2f40b6ed9e4255c8160d9bbfeb428f9d6b5
Axel Cymerman <axelcy@gmail.com>
2024-02-10 23:10:00 -0300

    Update gpt.ts

--- src/events/messageCreate/gpt.ts
+++ src/events/messageCreate/gpt.ts
@@ -1,12 +1,13 @@
 import { Client, Message } from "discord.js"
-import { OpenAI } from "openai"
+// import { OpenAI } from "openai"
 
-const openai = new OpenAI({ apiKey: process.env.OPENAI_KEY })
+// const openai = new OpenAI({ apiKey: process.env.OPENAI_KEY })
 
 export default async (client: Client, message: Message) => {
-    if (true) return
     // Error: 429 You exceeded your current quota
     // To use this again, please install the openai package: npm i openai
+    return
+    /*
     if (message.author.bot) return
     const CHANNELS = process.env.OPENAI_CHANNEL_ID?.split(',') || []
     if (!CHANNELS.includes(message.channel.id)) return
@@ -27,4 +28,5 @@ export default async (client: Client, message: Message) => {
     }).catch(error => console.error('OPENAI Error:\n' + error))
     const responseMessage = response?.choices[0].message.content
     if (responseMessage) message.reply(responseMessage || '')
+    */
 }
